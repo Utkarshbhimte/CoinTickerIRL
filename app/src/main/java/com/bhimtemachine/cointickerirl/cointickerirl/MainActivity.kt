3144589017eff7a277455209fc48d943ff24a373
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2017-12-18 17:39:20 +0530

    THe UI thread bug is solved
    and color is added on the basis of coin profit in last 24 hours

--- app/src/main/java/com/bhimtemachine/cointickerirl/cointickerirl/MainActivity.kt
+++ app/src/main/java/com/bhimtemachine/cointickerirl/cointickerirl/MainActivity.kt
@@ -14,6 +14,9 @@ import android.widget.Toast
 import com.android.volley.toolbox.Volley
 
 import android.content.Intent;
+import android.graphics.Color
+import android.os.Handler
+import android.os.Looper
 import android.view.View;
 
 import java.util.Timer;
@@ -24,6 +27,8 @@ import org.json.JSONObject;
 
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
+
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -51,51 +56,47 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-//        val coinData = kotlin.collections.ArrayList()
-
         //  Fetching new Coin data and updating it every 5 minutes
-        Timer().scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                fetchCoinData()
-            }
-        }, 0, 300000)
-
-
-//        Interval function to iterate over coinData
-//        Timer().scheduleAtFixedRate(object : TimerTask() {
-//            override fun run() {
-//                changeCoin ()
-//            }
-//        }, 0, 3000)
-
+        initiateSlide()
+        fetchCoinData()
     }
 
 
+    private var currentIndex = 0;
+
+    private var coinData = JSONArray()
+
     //  Function to fetch new coin data
     private fun fetchCoinData(){
-        val url = "https://api.coinmarketcap.com/v1/ticker/?limit=4"
 
+        val url = "https://api.coinmarketcap.com/v1/ticker/?limit=10 "
+
+        //  Initiating request queue here
         val requestQueue = Volley.newRequestQueue(this@MainActivity)
 
         val request = JsonArrayRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
                     Toast.makeText(this, "Data came through!", Toast.LENGTH_SHORT).show()
-//                    val newCoinData = kotlin.collections.ArrayList()
+
+                    //  Creating a empty JSONArray to switch with the current coinData JSONArray with new data
+                    val newCoinData = JSONArray()
 
                     for(i in 0..(response.length() - 1)){
                         val coinObject =  response.getJSONObject(i)
 
-                        val coinName = coinObject.optString("name").toString();
-                        val coinSymbol = coinObject.optString("symbol").toString();
-                        val price = coinObject.optString("price_usd").toString();
-                        val priceChange = coinObject.optString("percent_change_24h").toString();
+                        //  Creating another JSON Object to get only those keys which are usefull for the app
+                        val coin = JSONObject()
+                        coin.put("name", coinObject.optString("name").toString())
+                        coin.put( "symbol",coinObject.optString("symbol").toString())
+                        coin.put( "price_usd",coinObject.optString("price_usd").toString())
+                        coin.put( "percent_change_24h",coinObject.optString("percent_change_24h").toString())
 
-//                        newCoinData.add(coin("7.2", "San Francisco", "Feb 2, 2016"))
-
-                        android.util.Log.i("tag", "Coin Data:   $coinName ($coinSymbol): $price [$priceChange]")
+                        //  Adding to the JSONArray
+                        newCoinData.put(coin)
                     }
 
+                    //  Giving the new data to the main Object
+                    coinData = newCoinData
                 },
                 Response.ErrorListener {
                     e ->
@@ -105,16 +106,73 @@ class MainActivity : Activity() {
 
         requestQueue.add(request)
         requestQueue.start()
+
     }
 
-    //  Coin Class
-    fun coin (coinName: String, coinSymbol: String, price: String, priceChange: String){
+
+    //        Interval function to iterate over coinData
+    private fun initiateSlide(){
+
+            //  This looks if this is the first time since app has started or not, prevent multiple timers
+            if(currentIndex > 0) currentIndex = 0
+            else{
+                val changeCoinTimer = Timer()
+
+                changeCoinTimer.scheduleAtFixedRate(object : TimerTask() {
+                    override fun run() {
+                        //  changes coin iterating through index
+                        changeCoin (currentIndex)
+                    }
+                }, 0, 3000)
+            }
+
     }
 
+    //  Temp function to just show bitcoin price
+    fun showCoin(coinObject: JSONObject){
+        val coinName = coinObject.optString("name").toString()
+        val coinSymbol = coinObject.optString("symbol").toString()
+        val price = coinObject.optString("price_usd").toString()
+        val priceChange = coinObject.optString("percent_change_24h").toString()
+        val profitInLast24 = coinObject.optDouble("percent_change_24h").toDouble() > 0
+
+        val handler = Handler(Looper.getMainLooper()) // write in onCreate function
+
+        //below piece of code is written in function of class that extends from AsyncTask
+        handler.post(Runnable {
+            CoinLabel.text = "$coinName ($coinSymbol)   $priceChange"
+            PriceView.text = "$" + price
+
+            //  To give color on the basis of price change in last 24 hours
+            if(!!profitInLast24){
+                PriceView.setTextColor(Color.GREEN)
+            }else{
+                PriceView.setTextColor(Color.RED)
+            }
+        })
+
+    }
+
+    // class Coin
+    data class Coin(
+            val coinName: String,
+            val coinSymbol: String,
+            val price: String,
+            val priceChange: String
+    )
 
     //  Function that will change the text on the views with the next coin in the queue
-    fun changeCoin (){
-        android.util.Log.i("tag", "Coin change, mama")
+    fun changeCoin (index: Int){
+
+        if(coinData.length() > 0) {
+            val coinObject = coinData.getJSONObject(index)
+
+            showCoin(coinObject)
+
+            if(currentIndex < coinData.length() - 1) currentIndex++
+            else currentIndex = 0
+
+        }
     }
 
     //  Redirecting to the EditFormActivity
